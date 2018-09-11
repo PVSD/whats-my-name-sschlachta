a115ad0e20bba7f6c354a687fac21e230edff15d
sschlachta <ss772@pvsd.org>
2018-09-11 00:01:19 -0400

    Yews

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	import java.io.*;
+	import java.util.*;
+	public class FullName{
+
+		public static void main(String[] args){
+
+			Scanner fnReader = new Scanner(System.in);
+			System.out.println("What is your first name?");
+			String firstName = fnReader.nextLine();
+			System.out.println(" " + firstName);
+
+			Scanner lnReader = new Scanner(System.in);
+			System.out.print("What is your last name?");
+			String lastName = fnReader.nextLine();
+			System.out.println(" " + lastName);
+
+			Scanner fnReader = new Scanner(System.in);
+			System.out.print("Your full name is");
+			String fullName = fnReader.nextLine();
+			System.out.println(" " + fullName);
+		}
+    }
     }
 }
